125354cc6b460a2a6ac8e37d349384fbbb2a4336
yavuzselimpasa <yavuzselimpasa.2019@gmail.com>
2023-06-01 23:51:58 +0300

    01.06.2023/V.4

--- src/test/java/runners/ParalelRunner1.java
+++ src/test/java/runners/ParalelRunner1.java
@@ -6,9 +6,9 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-            plugin = {"html:target/cucumber-reports.html",
-                    "json:target/json-reports/cucumber.json",
-                    "junit:target/xml-report/cucumber.xml"
+            plugin = {"html:target/cucumber-reports1.html",
+                    "json:target/json-reports/cucumber1.json",
+                    "junit:target/xml-report/cucumber1.xml"
             },
             features = "src/test/resources/features",
             glue     = "stepdefinitions",
