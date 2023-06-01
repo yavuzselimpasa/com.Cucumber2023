0946eca5cf60a9400b6f9f6490103c40db0b4360
yavuzselimpasa <yavuzselimpasa.2019@gmail.com>
2023-06-01 23:47:47 +0300

    01.06.2023/V.4

---
+++ src/test/java/runners/ParalelRunner1.java
@@ -0,0 +1,13 @@
+package runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+public class ParalelRunner1 {
+
+    @RunWith(Cucumber.class)
+    @CucumberOptions(
+
+    )
+}
